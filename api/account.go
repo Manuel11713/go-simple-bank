21e240348b3b944ad3fd4ea9a56442a8321a5d91
Manuel Escobedo Lopez <manuelfx117@gmail.com>
2022-12-14 14:12:32 -0600

    implement users in the api

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/Manuel11713/simple-bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -30,6 +31,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, arg)
 
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation", "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
