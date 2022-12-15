2e39338d5de8ce72a5e32f4ff3b264130ca2d279
Manuel Escobedo Lopez <manuelfx117@gmail.com>
2022-12-15 01:03:26 -0600

    add users route

---
+++ utils/password_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(5)
+
+	hashedPassword, err := HashPassword(password)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(5)
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+}
